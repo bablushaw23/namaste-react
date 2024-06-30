a741a4c664a0e05433b5fade8a71ea8f12fd5f3e
bablushaw23 <46132062+bablushaw23@users.noreply.github.com>
2024-06-30 19:16:05 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,4 @@
+        // Create element in virtual DOM
+        const heading = React.createElement("h1", {id:"heading"}, "Hello world from React");
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(heading);
