d2a24006a166dd867a7649b87d32f4edad6ea9fe
bablushaw23 <46132062+bablushaw23@users.noreply.github.com>
2024-06-30 21:33:12 +0530

    adding parcel

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-        // Create element in virtual DOM
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create element in virtual DOM
         const heading = React.createElement("h1", {id:"heading"}, "Hello world from React");
         const root = ReactDOM.createRoot(document.getElementById("root"))
         root.render(heading);
